b5167dcb53d9c1fbb30b462c85c353d9b19b8269
Nishan Dhungana <nishandhungana41@hotmail.com>
2018-05-02 23:20:56 +0545

    Added model classes

---
+++ Bug Tracker/Model/Code.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bug_Tracker.Model
+{
+    /// <summary>
+    /// Model class for saving code
+    /// </summary>
+    class Code
+    {
+        public string CodeId { get; set; }
+        public string CodeFilePath { get; set; }
+        public string CodeFileName { get; set; }
+        public string ProgrammingLanguage { get; set; }
+    }
+}
